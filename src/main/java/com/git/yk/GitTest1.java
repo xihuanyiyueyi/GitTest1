01ea0d2c688eb475bf437232b4e25126f31b4e11
xihuanyiyueyi <2715587592@qq.com>
2022-07-26 14:10:52 +0800

    second commit

--- src/main/java/com/git/yk/GitTest1.java
+++ src/main/java/com/git/yk/GitTest1.java
@@ -3,5 +3,6 @@ package com.git.yk;
 public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
